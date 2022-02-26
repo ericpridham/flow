b9d055f01dd84f93a3f82f2c5ff532c447e93781
Eric Pridham <eric.pridham@continued.com>
2022-02-26 16:27:02 -0500

    Bug fix for path reorg. Kinda gross.

--- src/Watchers/RequestWatcher.php
+++ src/Watchers/RequestWatcher.php
@@ -5,6 +5,7 @@ namespace EricPridham\Flow\Watchers;
 use EricPridham\Flow\Flow;
 use EricPridham\Flow\Interfaces\FlowWatcher;
 use EricPridham\Flow\Payloads\RequestPayload;
+use EricPridham\Flow\Recorder\DatabaseRecorder;
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Support\Facades\Event;
 
@@ -13,7 +14,9 @@ class RequestWatcher implements FlowWatcher
     public function register(Flow $flow, array $params): void
     {
         Event::listen(RequestHandled::class, function (RequestHandled $event) use ($flow) {
-            if ($event->request->is(config('flow.path') . '*')) {
+            // hack for now. never record the flow path
+            $flowPath = config('flow.recorders')[DatabaseRecorder::class]['path']??'flow';
+            if ($event->request->is($flowPath . '*')) {
                 return;
             }
             $flow->record(RequestPayload::fromRequestHandled($event));
