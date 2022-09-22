d3d0efb940eef4dddd6e019264194a7c9cfa773d
Elanor Riley <oranges13@users.noreply.github.com>
2022-09-22 15:24:49 -0400

    Added tests and fixed tests

--- tests/Unit/Watchers/RequestWatcherTest.php
+++ tests/Unit/Watchers/RequestWatcherTest.php
@@ -32,7 +32,9 @@ class RequestWatcherTest extends FeatureTestCase
 
         $response = Mockery::mock(Response::class);
         $response->shouldReceive([
-            'getContent' => json_encode(['response' => true])
+            'getContent' => json_encode(['response' => true]),
+            'getStatusCode' => 200,
+            'isSuccessful' => true,
         ]);
 
         $flow->registerWatchers([new RequestWatcher()]);
@@ -45,6 +47,42 @@ class RequestWatcherTest extends FeatureTestCase
                 $this->assertEquals('method', $payload->data['method']);
                 $this->assertEquals(['foo' => 'bar'], $payload->data['contents']);
                 $this->assertEquals(['response' => true], $payload->data['response']);
+                return true;
+            }
+        );
+    }
+
+    public function test_it_replaces_long_requests(): void
+    {
+        $veryLongResponse = bin2hex(random_bytes(65535));
+
+        $flow = new Flow();
+        $recorder = Mockery::spy(FlowRecorder::class);
+        $flow->registerRecorders([$recorder]);
+
+        $request = \Mockery::mock(Request::class);
+        $request->shouldReceive([
+            'fullUrl' => 'full-url',
+            'method' => 'method',
+            'all' => ['foo' => 'bar'],
+            'is' => false,
+            'server' => 0.0
+        ]);
+
+        $response = Mockery::mock(Response::class);
+        $response->shouldReceive([
+            'getContent' => json_encode(['response' => $veryLongResponse]),
+            'getStatusCode' => 200,
+            'isSuccessful' => true,
+        ]);
+
+        $flow->registerWatchers([new RequestWatcher()]);
+
+        event(new RequestHandled($request, $response));
+
+        $recorder->shouldHaveReceived('record',
+            function (string $requestId, RequestPayload $payload) {
+                $this->assertEquals("Response Too Large", $payload->data['response']);
                 return true;
             }
         );
